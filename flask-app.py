e54f86f60a7491e7a0345a93197e23ff5fbea9f2
midoxc <minh-tri.do@polymtl.ca>
2022-09-24 20:43:31 -0400

    adding flask application
    Created a flask application that returns a small string containing the name of the ec2 instance.

---
+++ flask-app.py
@@ -0,0 +1,16 @@
+import os
+import requests
+from flask import Flask
+
+app = Flask(__name__)
+
+instance_id = requests.get('http://169.254.169.254/latest/meta-data/instance-id').text
+region = requests.get("http://169.254.169.254/latest/dynamic/instance-identity/document").json()['region']
+name = os.popen('aws ec2 describe-tags --region {} --filters "Name=resource-id,Values={}" "Name=key,Values=Name" --output text | cut -f5'.format(region, instance_id)).read()[:-1]
+
+@app.route('/')
+def hello_world():
+    return name + ' is responding now!'
+
+if __name__ == '__main__':
+      app.run(host='0.0.0.0', port=80)

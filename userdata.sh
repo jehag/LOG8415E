9c7565dc0a62aaed34a6e4b1d6e099123dbac58b
midoxc <60200767+midoxc@users.noreply.github.com>
2022-09-28 16:56:14 -0400

    Added userdata script
    added the user data script used by ec2 instances to setup the flask app on port 80

---
+++ userdata.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+apt-get update -y
+apt-get install python3-venv -y
+apt-get install nginx -y
+apt-get install awscli -y
+
+cd /home/ubuntu && git clone https://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@github.com/jehag/LOG8415E.git
+
+mkdir /home/ubuntu/.aws
+
+cp /home/ubuntu/LOG8415E/credentials /home/ubuntu/.aws
+cp /home/ubuntu/LOG8415E/flaskapp.service /etc/systemd/system
+
+cd /home/ubuntu/LOG8415E
+
+python3 -m venv venv
+source venv/bin/activate
+pip install -r requirements.txt
+
+systemctl daemon-reload
+systemctl start flaskapp
+systemctl enable flaskapp
+
+systemctl start nginx
+systemctl enable nginx
+
+cp /home/ubuntu/LOG8415E/default /etc/nginx/sites-available
+

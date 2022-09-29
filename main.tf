5d5e5aacb7ef847ce8c124b8bdb3475d988a51ed
jeanpaul87 <jp.khoueiry@gmail.com>
2022-09-29 18:05:23 -0400

    Adding instance creation

--- main.tf
+++ main.tf
@@ -12,3 +12,31 @@ terraform {
 provider "aws" {
   region  = "us-east-1"
 }
+
+resource "aws_instance" "cluster1_instances" {
+  count         = var.instance_count
+  ami           = "ami-0149b2da6ceec4bb0"
+  instance_type = var.instance_type[0]
+  availability_zone = var.availability_zone[count.index % 2]
+  user_data = "userdata.sh"
+  vpc_security_group_ids = [aws_security_group.not_secure_group.id]
+
+  tags = {
+    Name  = "Instance number ${count.index + 1}"
+  }
+}
+
+resource "aws_instance" "cluster2_instances" {
+  count         = var.instance_count
+  ami           = "ami-0149b2da6ceec4bb0"
+  instance_type = var.instance_type[1]
+  availability_zone = var.availability_zone[count.index % 2]
+  user_data = "userdata.sh"
+  vpc_security_group_ids = [aws_security_group.not_secure_group.id]
+
+
+  tags = {
+    Name  = "Instance number ${var.instance_count + count.index + 1}"
+  }
+}
+

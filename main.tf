c1a98d17d26b73c743cdd4d333841b2d8113cf26
jeanpaul87 <jp.khoueiry@gmail.com>
2022-09-29 18:09:12 -0400

    ALB setup for the two clusters

--- main.tf
+++ main.tf
@@ -69,3 +69,78 @@ resource "aws_security_group" "not_secure_group" {
     cidr_blocks = ["0.0.0.0/0"]
   }
 }
+
+resource "aws_alb" "alb" {
+  name = "alb"
+  subnets = data.aws_subnets.all.ids
+  security_groups = [aws_security_group.not_secure_group.id]
+}
+
+resource "aws_alb_target_group" "cluster1" {
+  name = "cluster1"
+  port = 80
+  protocol = "HTTP"
+  vpc_id = aws_vpc.vpc.id
+}
+
+resource "aws_alb_target_group" "cluster2" {
+  name = "cluster2"
+  port = 80
+  protocol = "HTTP"
+  vpc_id = aws_vpc.vpc.id
+}
+
+resource "aws_alb_listener" "listener" {
+  load_balancer_arn = aws_alb.alb.arn
+  port = 80
+  protocol = "HTTP"
+
+  default_action {    
+    target_group_arn = aws_alb_target_group.cluster1.arn
+    type             = "forward"  
+  }
+}
+
+resource "aws_alb_listener_rule" "cluster1_rule" {
+  listener_arn = aws_alb_listener.listener.arn
+  
+  action {
+    type = "forward"
+    target_group_arn = aws_alb_target_group.cluster1.arn
+  }
+
+  condition {
+    path_pattern {
+      values = ["/cluster1/*"]
+    }  
+  }
+}
+
+resource "aws_alb_listener_rule" "cluster2_rule" {
+  listener_arn = aws_alb_listener.listener.arn
+
+  action {
+    type = "forward"
+    target_group_arn = aws_alb_target_group.cluster2.arn
+  }
+
+  condition {
+    path_pattern {
+      values = ["/cluster2/*"]
+    }  
+  }
+}
+
+resource "aws_alb_target_group_attachment" "cluster1_external" {
+  for_each = {for id, attributes in aws_instance.cluster1_instances: id => attributes}
+  target_group_arn = aws_alb_target_group.cluster1.arn
+  target_id = each.value.id
+  port = 80
+}
+
+resource "aws_alb_target_group_attachment" "cluster2_external" {
+  for_each = {for id, attributes in aws_instance.cluster2_instances: id => attributes}
+  target_group_arn = aws_alb_target_group.cluster2.arn
+  target_id = each.value.id
+  port = 80
+}

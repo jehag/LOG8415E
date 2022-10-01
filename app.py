c523fa7fc3c59124474f6d6820bd551d0949d911
midoxc <minh-tri.do@polymtl.ca>
2022-10-01 12:35:19 -0400

    Removing aws as a dependency in the EC2s
    Returning instance ID in the message instead of the instance name

--- app.py
+++ app.py
@@ -5,13 +5,13 @@ from flask import Flask
 app = Flask(__name__)
 
 instance_id = requests.get('http://169.254.169.254/latest/meta-data/instance-id').text
-region = requests.get("http://169.254.169.254/latest/dynamic/instance-identity/document").json()['region']
-name = os.popen('aws ec2 describe-tags --region {} --filters "Name=resource-id,Values={}" "Name=key,Values=Name" --output text | cut -f5'.format(region, instance_id)).read()[:-1]
+# region = requests.get("http://169.254.169.254/latest/dynamic/instance-identity/document").json()['region']
+# name = os.popen('aws ec2 describe-tags --region {} --filters "Name=resource-id,Values={}" "Name=key,Values=Name" --output text | cut -f5'.format(region, instance_id)).read()[:-1]
 
 @app.route('/', defaults={'path': ''})
 @app.route('/<path:path>')
 def hello(path = None):
-    return name + ' is responding now!'
+    return instance_id + ' is responding now!'
 
 if __name__ == '__main__':
       app.run()

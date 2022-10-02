1235f82af5ac27692e7a6278aa20f9a3b8a02d7f
midoxc <minh-tri.do@polymtl.ca>
2022-10-01 23:00:44 -0400

    fixed a typo

--- scripts.sh
+++ scripts.sh
@@ -1,5 +1,5 @@
 terraform init
 terraform apply -auto-approve
-docker build -t python_container  .
+docker build -t python_container .
 docker run -e URL=$(terraform output dns_address) python_container
 $SHELL

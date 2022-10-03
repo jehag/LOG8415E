eb00d0184632e37f370f124ef9c33536bf3f3b9e
jehag <jeremy.hage@polymtl.ca>
2022-10-03 18:39:22 -0400

    start of run all script

--- dockerfile
+++
@@ -1,7 +0,0 @@
-FROM hashicorp/terraform:latest
-
-WORKDIR /usr/app/src
-
-COPY scripts.sh ./
-
-CMD ["./scripts.sh"]
--- scripts.sh
+++ scripts.sh
@@ -1,5 +1,6 @@
 terraform init
 terraform apply -auto-approve
 docker pull bawsje/log8415e_1:latest
-docker run -e URL=$(terraform output --raw dns_address) bawsje/log8415e_1:latest
+DOCKER_ID=$(docker run -d -e URL=$(terraform output --raw dns_address) bawsje/log8415e_1:latest)
+docker logs -f $DOCKER_ID
 $SHELL

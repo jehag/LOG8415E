bdba8cd81dee2981fd2f97cf25906580cb068178
jeanpaul87 <jp.khoueiry@gmail.com>
2022-10-06 13:13:04 -0400

    updating delays and debug mesasges

--- script.sh
+++ script.sh
@@ -9,18 +9,35 @@ sleep 10
 
 printf '%*s\n' "${COLUMNS:-80}" '' | tr ' ' -
 
-export BENCHMARK_START=$(date +%s) 
+# save the requests start time for benchmarking purposes
+export REQUESTS_START=$(date +%s) 
 
-python3 send_requests.py $(terraform output --raw dns_address)
-
+# send the requests to each cluster
+python send_requests.py $(terraform output --raw dns_address)
 
 printf '%*s\n' "${COLUMNS:-80}" '' | tr ' ' -
 
 printf 'Benchmarking ...\n'
-# delay to wait for last requests metrics to update
 
-python3 benchmark.py
+# save the benchmark start time for benchmarking purposes
+export BENCHMARK_START=$(date +%s) 
+
+# delay to wait for the requests metrics to update
+sleep 60
+
+# get the metrics data between REQUESTS_START and the current time
+python benchmark.py 
 
 printf '%*s\n' "${COLUMNS:-80}" '' | tr ' ' -
 
+printf '60 seconds before destroying resources ...\n'
+
+sleep 60
+
+printf 'destroying resources ...\n'
+
 terraform destroy -auto-approve > /dev/null
+
+printf 'destroyed resources\n'
+
+sleep 10
